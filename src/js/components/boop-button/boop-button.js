cf6603335813da8e8cf385e89870255b9a017a36
Aaren Bertilsson <ab226gw@student.lnu.se>
2024-04-04 08:33:03 +0200

    Add a functional boop

--- src/js/components/boop-button/boop-button.js
+++ src/js/components/boop-button/boop-button.js
@@ -1,8 +1,20 @@
 const template = document.createElement('template')
 template.innerHTML = `
+<style>
+    #boopContainer {
+      display: none;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+  </style>
   <form>
     <input type="submit" value="Boop">
   </form>
+  <div id="boopContainer">
+    <img id="boopGif" src="./images/boop1.gif" alt="boop">
+  </div>
 `
 customElements.define('boop-button',
   /**
@@ -10,6 +22,8 @@ customElements.define('boop-button',
    */
   class extends HTMLElement {
     #form
+    #boopContainer
+    #boopGif
     /**
      * Creates an instance of the current type.
      */
@@ -20,6 +34,8 @@ customElements.define('boop-button',
         .appendChild(template.content.cloneNode(true))
 
       this.#form = this.shadowRoot.querySelector('form')
+      this.#boopContainer = this.shadowRoot.querySelector('#boopContainer')
+      this.#boopGif = this.shadowRoot.querySelector('#boopGif')
 
       this.#listenForSubmit()
     }
@@ -30,7 +46,20 @@ customElements.define('boop-button',
     #listenForSubmit () {
       this.#form.addEventListener('submit', (event) => {
         event.preventDefault()
+        this.#displayBoop()
       })
+    }
+
+    /**
+     * Plays the boop animation once.
+     */
+    #displayBoop () {
+      this.#boopGif.currentTime = 0
+      this.#boopContainer.style.display = 'block'
+
+      setTimeout(() => {
+        this.#boopContainer.style.display = 'none'
+      }, 1700) // Hide after 1 second
     }
   }
 )
